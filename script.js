19e3c945dbd8d4eeb3ac88d163c99e11e74a6e29
isaacjacques <jacques.development@outlook.com>
2025-06-09 16:41:46 -0400

    rmv color enumerator

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const hour = new Date().getHours();
 const imageElement = document.getElementById("timeImage");
 let imageFile;
 
-const COLORS = {
-    morning: "#994d00",    
-    afternoon: "#007399", 
-    default: "#260099"
-};
-
 if (hour >= 6 && hour < 12) {
     imageFile = "morning.jpg";
 } else if (hour >= 12 && hour < 18) {
